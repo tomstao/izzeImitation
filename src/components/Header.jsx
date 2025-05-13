bb048f1863d0e1b5dccd05ff5d497ec20426056b
Tao Su <96sutao@gmail.com>
2025-05-13 18:24:30 -0400

    Header draft

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import Btn from "./Buttons.jsx";
+
 function Header() {
     function Nav() {
 
-        return(
+        return (
             <>
                 <nav className="navbar header-nav d-flex justify-content-between align-items-center p-4">
-                    <h1 className="navbar-brand fs-2"><span className='nav-title' style={{ fontStyle: 'italic', fontFamily: 'cursive' }}>Tao's dessert</span></h1>
+                    <h1 className="navbar-brand fs-2"><span className='nav-title'
+                                                            style={{fontStyle: 'italic', fontFamily: 'cursive'}}>Tao's dessert</span>
+                    </h1>
                     <button className="btn btn-outline-light fs-2 menu">Menu</button>
                 </nav>
             </>
@@ -15,19 +18,19 @@ function Header() {
 
     return (
         <>
-        <header className="text-white py-3 header">
-            <div className="container row">
-            <Nav/>
-                <div className="left-btn-wrapper">
-                    <Btn className="header-btn">{'NEW items'}</Btn>
-                    <Btn className="header-btn">{'Popular sweets'}</Btn>
+            <header className="text-white py-3 header">
+                <div className="container row">
+                    <Nav/>
+                    <div className="left-btn-wrapper">
+                        <Btn className="header-btn">{'NEW items'}</Btn>
+                        <Btn className="header-btn">{'Popular sweets'}</Btn>
+                    </div>
+                    <div className='container mt-5 quote-container row px-lg-5'>
+                        <h1 className='blockquote quote-title quote row'>MEET OUR</h1>
+                        <h1 className='blockquote quote-subtitle quote row'>Sweets</h1>
+                    </div>
                 </div>
-                <div className='container mt-5 quote-container row px-lg-5'>
-                    <h1 className='blockquote quote-title quote row'>MEET OUR</h1>
-                    <h1 className='blockquote quote-subtitle quote row'>Sweets</h1>
-                </div>
-            </div>
-        </header>
+            </header>
         </>
     );
 }
