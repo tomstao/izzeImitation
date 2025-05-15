c3e79fff775ebd2ec2a17c745c35bfdae41ff0fd
Tao Su <96sutao@gmail.com>
2025-05-15 09:12:18 -0400

    added dessert db

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,41 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-CORS(app)  # allows React to connect to Flask
+CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://sutommy:9750@localhost:5432/desserts_db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text, nullable=False)
+    description = db.Column(db.Text)
+    price = db.Column(db.Numeric(10, 2), nullable=False)
+    image_url = db.Column(db.Text)
+    is_popular = db.Column(db.Boolean, default=False)
 
 @app.route('/api/greeting')
 def greet():
-    return jsonify(message="Hello from Flask!")
+    return jsonify(message="Enjoy the sweetness!")
+
+@app.route('/api/products')
+def get_products():
+    products = Product.query.all()
+    return jsonify([
+        {
+            "id": p.id,
+            "name": p.name,
+            "description": p.description,
+            "price": float(p.price),
+            "image_url": p.image_url,
+            "is_popular": p.is_popular
+        }
+        for p in products
+    ])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
