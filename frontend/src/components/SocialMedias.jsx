7462bf80821d531652045e3def1693175b467d48
Tao Su <96sutao@gmail.com>
2025-05-17 19:34:18 -0400

    Start working on Footer.jsx

---
+++ frontend/src/components/SocialMedias.jsx
@@ -0,0 +1,22 @@
+import {FaFacebook, FaTwitter, FaInstagram} from 'react-icons/fa';
+
+function SocialMedias() {
+
+    return (
+        <>
+            <div className="social-icons">
+                <a href="https://facebook.com" target="_blank" rel="noreferrer">
+                    <FaFacebook size={28}/>
+                </a>
+                <a href="https://twitter.com" target="_blank" rel="noreferrer">
+                    <FaTwitter size={28}/>
+                </a>
+                <a href="https://instagram.com" target="_blank" rel="noreferrer">
+                    <FaInstagram size={28}/>
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default SocialMedias;
