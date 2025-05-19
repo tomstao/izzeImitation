ca9bb3c7ad017acf4a8a347fc2f816e83bf7bfdb
Tao Su <96sutao@gmail.com>
2025-05-19 10:46:47 -0400

    Main page framework completed

--- frontend/src/components/MiddleTransition.jsx
+++ frontend/src/components/MiddleTransition.jsx
@@ -8,7 +8,7 @@ function MiddleTransition() {
         <>
             <div className="text-center mt-5 pt-5 row align-items-center justify-content-center">
                 <div className='row'>
-                <h1 className='middle-transition'>MAKE IT A SWEET MOMENT</h1>
+                <h1 className='middle-transition middle-transition-title'>MAKE IT A SWEET MOMENT</h1>
                 <p className='middle-transition'>#dessert #tao'sdessert #feelthesweet</p>
                 </div>
                 <div className="w-25 row">
--- frontend/src/components/ScrollingBanner.jsx
+++ frontend/src/components/ScrollingBanner.jsx
@@ -1,19 +1,19 @@
 import '/src/css/ScrollingBanner.css'
-import {FaBirthdayCake, FaPercent, FaTruck} from 'react-icons/fa';
+import {FaBirthdayCake, FaTiktok, FaTruck} from 'react-icons/fa';
 
 function ScrollingBanner() {
     return (
         <>
             <div className="scroll-banner">
                 <div className="scroll-track">
-                    <span><FaBirthdayCake/>&nbsp; Try our new summer desserts! &nbsp;&nbsp;&nbsp;</span>
-                    <span>&nbsp; 20% off all cakes this weekend! &nbsp;&nbsp;&nbsp;</span>
-                    <span><FaTruck/>&nbsp; Free delivery over $25! &nbsp;&nbsp;&nbsp;</span>
+                    <span><FaBirthdayCake />&nbsp; Try our new summer desserts! &nbsp;&nbsp;&nbsp;</span>
+                    <span><FaTiktok />&nbsp; Follow us on TikTok for exclusive deals! &nbsp;&nbsp;&nbsp;</span>
+                    <span><FaTruck />&nbsp; Free delivery over $25! &nbsp;&nbsp;&nbsp;</span>
 
                     {/* repeat to enable smooth loop */}
-                    <span><FaBirthdayCake/>&nbsp; Try our new summer desserts! &nbsp;&nbsp;&nbsp;</span>
-                    <span>&nbsp; 20% off all cakes this weekend! &nbsp;&nbsp;&nbsp;</span>
-                    <span><FaTruck/>&nbsp; Free delivery over $25! &nbsp;&nbsp;&nbsp;</span>
+                    <span><FaBirthdayCake />&nbsp; Try our new summer desserts! &nbsp;&nbsp;&nbsp;</span>
+                    <span><FaTiktok />&nbsp; Follow us on TikTok for exclusive deals! &nbsp;&nbsp;&nbsp;</span>
+                    <span><FaTruck />&nbsp; Free delivery over $25! &nbsp;&nbsp;&nbsp;</span>
                 </div>
             </div>
         </>
